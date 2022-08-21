f1c524ecfd946e300675faf4e340bf8f1cfb58e7
Agustin Hernandez Montero <72844451+lagus11@users.noreply.github.com>
2022-08-21 17:42:20 -0600

    Add files via upload
    agregaron animaciones

--- animation/main.js
+++ animation/main.js
@@ -1,4 +1,4 @@
-let animation = document.getElementById('sobre_mi');
+/*let animation = document.getElementById('sobre_mi');
     let animationImg = document.getElementById('img_sobre_mi');
     let posicionObj1 = animation.getBoundingClientRect().top;
     let tamPantalla = this.window.innerHeight * 0.7;
@@ -17,3 +17,26 @@ window.addEventListener('scroll', function(){
         animation3.style.animation = 'mover 1s ease-out';
     }
 })
+*/
+
+const imagen0 = document.getElementById('sobre_mi');
+const imagen1 = document.getElementById('container_habilidades');
+const imagen2 = document.getElementById('container_proyectos');
+
+const cargarImagen = (entradas, obsercador) => {
+    entradas.forEach((entrada) => {
+        if(entrada.isIntersecting){
+            entrada.target.classList.add('visible');
+        }
+    });
+}
+
+const oservador = new IntersectionObserver(cargarImagen, {
+    root: null, 
+    rootMargin: '0PX',
+    threshold: 0.1
+});
+
+oservador.observe(imagen0);
+oservador.observe(imagen1);
+oservador.observe(imagen2);
